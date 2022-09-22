6ea951677d7227f32eb4b0892280c1100ba9ca1e
ooiippuu <95650879+ooiippuu@users.noreply.github.com>
2022-09-22 22:00:02 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': '52d2118a99', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['201069401225']
+global.premium = ['201069401225']
+global.ownernomer = '201069401225'
+global.ownername = '( الـمـطـور )'
+global.botname = '( بـوت لورا )'
+global.footer = '( لورا )'
+global.ig = 'https://github.com/votlx1'
+global.region = 'egypt, South Africa, Kerala'
+global.sc = 'https://chat.whatsapp.com/ISalhpLNQb357EKumkHCXZ'
+global.myweb = 'https://t.me/MMOD_X'
+global.packname = 'https://Wa.Me/+201069401225'
+global.author = '[ لورا ]'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓',
+    admin: '*「 هـذا الامـر فـقـط لـ المـشـرفـيـن 」◣*',
+    botAdmin: '*「 اعـطـنـي إشـراف لـتـفـعـيـل  الامـر 」◣*',
+    owner: '*「 هـذا الامـر فـقـط لـ الـمـالـك 」◣*',
+    group: '*「 هـذا الامـر فـي الـمـجـمـوعـات فـقـط 」◣*',
+    private: '*「 هـذا الامـر فـي الـخـاص فـقـط 」◣*',
+    bot: '*「 هـذا الامـر مـخـصـص لـ الـبـوت 」◣*',
+    wait: '*「 انـتـظـر مـن فـضـلـك 」◣*',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 1000 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./GojoMedia/gojo.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
